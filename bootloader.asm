81a2061ea4fbf8ec06bd6144e48bc6fb101f424b
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2018-04-06 20:44:44 +0200

    Add license boilerplate comment.

--- bootloader.asm
+++ bootloader.asm
@@ -1,5 +1,17 @@
 ; --------------------------------------------------------------------------------------------
 ; Phobis bootloader. Copyright (C) 2018 by Krzysztof Szewczyk. All rights reserved!
+;
+;   Licensed under the Apache License, Version 2.0 (the "License");
+;   you may not use this file except in compliance with the License.
+;   You may obtain a copy of the License at
+;
+;       http://www.apache.org/licenses/LICENSE-2.0
+;
+;   Unless required by applicable law or agreed to in writing, software
+;   distributed under the License is distributed on an "AS IS" BASIS,
+;   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;   See the License for the specific language governing permissions and
+;	limitations under the License.
 
 
 org 0x7C00													; Entry point for every bootloader
